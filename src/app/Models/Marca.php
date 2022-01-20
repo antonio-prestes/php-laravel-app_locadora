ee75785a6b2c8923b28700aa034a1d2e82bdfe32
Diego <diegoantoniodasp@hotmail.com>
2022-01-20 11:09:05 -0300

    299. Implementando as regras de validação (Rules) no Model

--- src/app/Models/Marca.php
+++ src/app/Models/Marca.php
@@ -8,9 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Marca extends Model
 {
     use HasFactory;
+
     protected $fillable = ['nome', 'imagem'];
     /**
      * @var mixed
      */
     private $id;
+
+    public function rules()
+    {
+        return [
+            'nome' => 'required|unique:marcas|min:3',
+            'imagem' => 'required'
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.unique' => 'Marca já existe',
+            'nome.min' => 'O nome deve ter no minimo 3 caracteres'
+        ];
+    }
 }
