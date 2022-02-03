63ad18b62931d72e675a2cf52d728e424b96b197
diegoantonioprestes <101010diego101010>
2022-02-03 15:48:05 -0300

    321. API WebService Rest para o Resource Carro

--- src/app/Http/Controllers/CarroController.php
+++ src/app/Http/Controllers/CarroController.php
@@ -3,18 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\Carro;
+use App\Repositories\CarroRepository;
 use Illuminate\Http\Request;
 
 class CarroController extends Controller
 {
+    public function __construct(Carro $carro)
+    {
+        $this->carro = $carro;
+    }
+
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $carroRepository = new CarroRepository($this->carro);
+
+        if ($request->has('atributos_modelo')) {
+            $atributos_modelo = 'modelo:id,' . $request->atributos_modelo;
+            $carroRepository->selectAtributosRegistrosRelacionados($atributos_modelo);
+        } else {
+            $carroRepository->selectAtributosRegistrosRelacionados('modelo');
+        }
+
+        if ($request->has('filtro')) {
+            $carroRepository->filtro($request->filtro);
+        }
+
+        if ($request->has('atributos')) {
+            $carroRepository->selectAtributos($request->atributos);
+        }
+
+        return response()->json($carroRepository->getResultados(), 200);
     }
 
     /**
@@ -30,29 +53,44 @@ class CarroController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->carro->rules());
+
+        $carro = $this->carro->create([
+            'modelo_id' => $request->modelo_id,
+            'placa' => $request->placa,
+            'disponivel' => $request->disponivel,
+            'km' => $request->km
+        ]);
+        return $carro;
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Carro  $carro
-     * @return \Illuminate\Http\Response
+     * @param \App\Models\Carro $carro
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response
      */
-    public function show(Carro $carro)
+    public function show($id)
     {
-        //
+        $carro = $this->carro->with('modelo')->find($id);
+
+        if ($carro === null) {
+            return response()->json(['erro' => 'Carro não existe'], 404);
+        } else {
+            return $carro;
+        }
     }
+
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Carro  $carro
+     * @param \App\Models\Carro $carro
      * @return \Illuminate\Http\Response
      */
     public function edit(Carro $carro)
@@ -63,23 +101,50 @@ class CarroController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Carro  $carro
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Carro $carro
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Carro $carro)
+    public function update(Request $request, $id)
     {
-        //
+        $carro = $this->carro->find($id);
+        if ($carro === null) {
+            return response()->json(['erro' => 'Atualização não realizada, marca não existe'], 404);
+        }
+
+        if ($request->method() === "PATCH") {
+            $regrasDinamicas = array();
+
+            foreach ($carro->rules() as $input => $regra) {
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+            $request->validate($regrasDinamicas);
+        } else {
+            $request->validate($carro->rules());
+        }
+
+        $carro->fill($request->all());
+        $carro->save();
+
+        return $carro;
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Carro  $carro
+     * @param \App\Models\Carro $carro
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Carro $carro)
+    public function destroy($id)
     {
-        //
+        $carro = $this->carro->find($id);
+
+        if ($carro === null) {
+            return response()->json(['erro' => 'Deleção não realizada, carro não existe'], 404);
+        }
+        $carro->delete();
+        return ['msg' => 'Carro deletada com sucesso.'];
     }
 }
--- src/app/Models/Carro.php
+++ src/app/Models/Carro.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Carro extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["modelo_id", "placa", "disponivel", "km"];
+
+    public function rules()
+    {
+        return [
+           // 'modelo_id' = 'exists:modelos,id',
+            'placa' => 'required' ,
+            'km' => 'required',
+            'disponivel' => 'required|boolean',
+        ];
+    }
+
+    public function modelo()
+    {
+        //um carro para um modelo
+       return $this->belongsTo('App\Models\Modelo');
+    }
 }
